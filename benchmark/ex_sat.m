a1e26a0957325175f7438c9c25f6ac05911a1277
Erdenebayar2 <130644894+Erdenebayar2@users.noreply.github.com>
2025-09-04 11:19:35 +0200

    Add files via upload

---
+++ benchmark/ex_sat.m
@@ -0,0 +1,7 @@
+x = sdpvar(1,2);
+ex_qtf= [2];
+un_qtf = [1];
+ranges = [1,3-x(1), 3+x(1)];
+xdeg = 1;
+f= x(2)-x(1)
+rest = []
---
+++ benchmark/ex_sat3.m
@@ -0,0 +1,8 @@
+x = sdpvar(1,6);
+ex_qtf= [2];
+un_qtf = [1];
+ranges = [3-x(1), 3+x(1)];
+xdeg = 1;
+f= 1;
+rest = [];
+
