9f48ec5befc2a50fdd7cbd1e51e72e4c9bb2be19
Erdenebayar2 <130644894+Erdenebayar2@users.noreply.github.com>
2025-12-03 22:39:36 +0800

    Add files via upload

---
+++ benchmark/Example54.m
@@ -0,0 +1,9 @@
+% Auto-generated polynomial file
+
+x = sdpvar(1, 10);
+ex_qtf = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+un_qtf = [10];
+ranges = [1, 1 - x(10), 1 + x(10)];
+xdeg = 1;
+logical_formula = {{1*x(5)-5-5*x(9)-4*x(8)+4*x(3), -1*x(8)-3-1*x(1)-4*x(3), 3-2*x(2)}, {-2+2*x(9), 1*x(7), 3*x(3)-2*x(9)-3*x(4)+1-1*x(6)-2*x(7)}};
+
